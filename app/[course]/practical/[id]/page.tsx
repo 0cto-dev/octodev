b3654b6fb6b742745d533e567b06c09e4e4d617c
renanrod4 <renanrdemeneses@gmail.com>
2025-10-13 00:38:32 -0300

    fix(exercicios) arrumando a detecção de erro

--- app/[course]/practical/[id]/page.tsx
+++ app/[course]/practical/[id]/page.tsx
@@ -15,14 +15,14 @@ export default function Home({ params }: { params: Promise<paramsType> }) {
     useEffect(() => {
         fetchData(paramsObj, setLesson, setLoaded);
     }, [paramsObj]);
+    if (!lesson.data) return <h1>ERRO, A LIÇÃO QUE VOCÊ TENTOU ACESSAR NÃO EXISTE</h1>
+    if (!lesson.data.exercicios) return <h1>ERRO, A LIÇÃO QUE VOCÊ TENTOU ACESSAR NÃO EXISTE</h1>
     
-    const totalExercises = lesson.data?.exercicios.length;
+    const totalExercises = lesson.data.exercicios.length;
     const currentExercise = 1;
     const completedExercises = currentExercise - 1;
     const lessonProgress = (completedExercises / totalExercises) * 100;
-    const shuffledAlternatives = loaded? shuffle(lesson.data?.exercicios[currentExercise - 1]?.alternativas||[]) as alternativasType[]:[];
-    if (!lesson.data) return <h1>ERRO, A LIÇÃO QUE VOCÊ TENTOU ACESSAR NÃO EXISTE</h1>
-    if (!lesson.data.exercicios) return <h1>ERRO, A LIÇÃO QUE VOCÊ TENTOU ACESSAR NÃO EXISTE</h1>
+    const shuffledAlternatives = loaded? shuffle(lesson.data.exercicios[currentExercise - 1].alternativas) as alternativasType[]:[];
     return (
 loaded&&<main>
             <NavBar data={lesson.data} currentExercise={currentExercise} totalExercises={totalExercises} lessonProgress={lessonProgress} />
