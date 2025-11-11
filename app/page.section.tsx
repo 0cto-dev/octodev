2049661bc4082283b6f590b41237509cfbb3f642
renanrod4 <renanrdemeneses@gmail.com>
2025-11-10 23:40:33 -0300

    feat/style(main) adiciona searchbar e diminui o tamanho dos cards

--- app/page.section.tsx
+++ app/page.section.tsx
@@ -1,5 +1,7 @@
 import { courseType, lessonType } from '@/types/types';
 import Course from './page.course';
+import { IoSearch } from 'react-icons/io5';
+import { RiArrowDropDownLine } from 'react-icons/ri';
 
 export default function Section({
 	avaliableCourses,
@@ -10,24 +12,48 @@ export default function Section({
 }) {
 	return (
 		<section>
-			{avaliableCourses
-				.toSorted((a, b) => {
-					if (a.disponivel && !b.disponivel) return -1;
-					if (!a.disponivel && b.disponivel) return 1;
+			<SearchBar />
+			<div className="coursesContent">
+				{avaliableCourses
+					.toSorted((a, b) => {
+						if (a.disponivel && !b.disponivel) return -1;
+						if (!a.disponivel && b.disponivel) return 1;
 
-					return a.nome.localeCompare(b.nome);
-				})
-				.map((course, i) => {
-					return (
-						<Course
-							key={i}
-							course={course}
-							LessonsNum={
-								courses.toSorted((a, b) => a.course.localeCompare(b.course))[i]?.data.length || 0
-							}
-						/>
-					);
-				})}
+						return a.nome.localeCompare(b.nome);
+					})
+					.map((course, i) => {
+						return (
+							<Course
+								key={i}
+								course={course}
+								LessonsNum={
+									courses.toSorted((a, b) => a.course.localeCompare(b.course))[i]?.data.length || 0
+								}
+							/>
+						);
+					})}
+			</div>
 		</section>
+	);
+}
+
+export function SearchBar() {
+	return (
+		<div className="searchBarArea">
+			<div className="searchBar">
+				<input type="text" placeholder="Pesquisar por cursos..." id="searchBar" />
+				<button id="filter">
+					Filtrar
+					<RiArrowDropDownLine size={25} />
+				</button>
+				<button className="focus">
+					<IoSearch size={25} />
+				</button>
+			</div>
+			<button id="order">
+				Ordenar
+				<RiArrowDropDownLine size={25} />
+			</button>
+		</div>
 	);
 }
