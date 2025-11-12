f7dc92049b302de008b33d4667457072125d5209
renanrod4 <renanrdemeneses@gmail.com>
2025-11-11 22:13:19 -0300

    feat(main) cria o dropDown component

--- app/page.section.tsx
+++ app/page.section.tsx
@@ -2,6 +2,7 @@ import { courseType, lessonType } from '@/types/types';
 import Course from './page.course';
 import { IoSearch } from 'react-icons/io5';
 import { RiArrowDropDownLine } from 'react-icons/ri';
+import { useState } from 'react';
 
 export default function Section({
 	avaliableCourses,
@@ -42,18 +43,70 @@ export function SearchBar() {
 		<div className="searchBarArea">
 			<div className="searchBar">
 				<input type="text" placeholder="Pesquisar por cursos..." id="searchBar" />
-				<button id="filter">
+				<DropDownMenu
+					id="filter"
+					items={[
+						{ name: 'Fácil', handler: () => {} },
+						{ name: 'Médio', handler: () => {} },
+						{ name: 'Difícil', handler: () => {} },
+						{ name: 'Disponível', handler: () => {} },
+						{ name: 'Não concluido', handler: () => {} },
+					]}
+				>
 					Filtrar
-					<RiArrowDropDownLine size={25} />
-				</button>
+				</DropDownMenu>
+
 				<button className="focus">
 					<IoSearch size={25} />
 				</button>
 			</div>
-			<button id="order">
+			<DropDownMenu
+				id="order"
+				items={[
+					{ name: 'Ordem alfabética', handler: () => {} },
+					{ name: 'Dificuldade', handler: () => {} },
+					{ name: 'Progresso', handler: () => {} },
+				]}
+			>
 				Ordenar
+			</DropDownMenu>
+		</div>
+	);
+}
+type DropDownMenuType = { children: React.ReactNode; id: string; items: { name: string; handler: () => void }[] };
+
+function DropDownMenu({ children, id, items }: DropDownMenuType) {
+	const [isOpen, setIsOpen] = useState(false);
+	const [currentItem, setCurrentItem] = useState(-1);
+
+	function handleClick(i: number = -1) {
+		if (isOpen) {
+			i >= 0 && items[i].handler();
+			setCurrentItem(currentItem => (i === currentItem ? -1 : i));
+		}
+		
+		setIsOpen(isOpen => !isOpen);
+	}
+	return (
+		<>
+			<button id={id} onClick={() => handleClick()}>
+				{currentItem === -1 ? children : items[currentItem]?.name}
 				<RiArrowDropDownLine size={25} />
 			</button>
-		</div>
+
+			{isOpen && (
+				<ul className='options'>
+					{items.map((item, i) => {
+						const selected = item.name === items[currentItem]?.name;
+						console.log(item)
+						return (
+							<li key={i} onClick={() => handleClick(i)}>
+								<button className={`option ${selected ? 'selected' : ''}`}>{item.name}</button>
+							</li>
+						);
+					})}
+				</ul>
+			)}
+		</>
 	);
 }
