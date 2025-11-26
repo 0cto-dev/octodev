3528541f72ca32cc108f16376f8adf032bb1e687
renanrod4 <renanrdemeneses@gmail.com>
2025-11-25 21:48:20 -0300

    style(login) aprimora o estilo da página de login

--- app/(with-nav)/login/page.tsx
+++ app/(with-nav)/login/page.tsx
@@ -1,34 +1,62 @@
 'use client';
 import './page.css';
 import { signIn, useSession } from 'next-auth/react';
-import { FcGoogle } from "react-icons/fc";
-import { FiGithub } from "react-icons/fi";
-import { FaLinkedinIn } from "react-icons/fa";
+import { FcGoogle } from 'react-icons/fc';
+import { FiGithub } from 'react-icons/fi';
+import { FaLinkedinIn } from 'react-icons/fa';
+import { useEffect, useState } from 'react';
+import Link from 'next/link';
 
 export default function LoginPage() {
-	const { data: session } = useSession();
-
-	console.log('SESSION:', session);
+	// const { data: session } = useSession();
+	// const [isRedirecting, setIsRedirecting] = useState(false);
+	// const [isLoading, setIsLoading] = useState(true);
+	// useEffect(() => {
+	// 	if (!window ) return;
+	// 	if (session) {
+	// 		setIsRedirecting(true);
+	// 		window.location.href = '/';
+	// 		console.log(!isLoading, !isRedirecting);
+	// 	}else{
+	// 		setIsLoading(false);
+	// 	}
+	// }, [session]);
+	// if (isRedirecting || isLoading) {
+	// 	return null;
+	// }
 
 	return (
-		<main>
-			<div className="title">
-				<h1>Fazer login!</h1>
-			</div>
-
-			<div className="buttons">
-				<button onClick={() => signIn('google')} className="googleBtn button">
-          <FcGoogle size={40}/>
-					Entrar com Google
-				</button>
-				<button onClick={() => {}} className="githubBtn button">
-          <FiGithub size={40}/>
-					Entrar com GitHub
-				</button>
-        <button onClick={() => {}} className="linkedinBtn button">
-          <FaLinkedinIn size={35}/>
-					Entrar com LinkedIn
-				</button>
+		<main className="loginPage">
+			<div className="main">
+				<div className="title">
+					<h1>Fazer login</h1>
+					<p>No OctoDev você aprende brincando!</p>
+				</div>
+				<div className="divisor">
+					<div className="division"></div>
+					<span>ENTRE COM</span>
+					<div className="division"></div>
+				</div>
+				<div className="buttons">
+					<button onClick={() => signIn('google')} className="googleBtn button">
+						<FcGoogle size={25} />
+						Entrar com Google
+					</button>
+					<button onClick={() => {}} className="githubBtn button">
+						<FiGithub size={25} />
+						Entrar com GitHub
+					</button>
+					<button onClick={() => {}} className="linkedinBtn button">
+						<FaLinkedinIn size={20} />
+						Entrar com LinkedIn
+					</button>
+				</div>
+				<footer>
+					<p>
+						Ao continuar, você concorda com nossos <Link href={`/termos`}>Termos de Uso</Link> e{' '}
+						<Link href={`/privacidade`}>Política de Privacidade</Link>
+					</p>
+				</footer>
 			</div>
 		</main>
 	);
---
+++ app/(with-nav)/privacidade/page.tsx
@@ -0,0 +1,38 @@
+import './page.css';
+export default function PrivacidadePage() {
+	return (
+		<main className="privacidadePage">
+			<div className="content">
+				<h1>Política de Privacidade</h1>
+				<p>
+					A sua privacidade é de extrema importância para nós. Esta Política de Privacidade descreve como
+					coletamos, usamos e armazenamos suas informações pessoais ao utilizar nossa plataforma!
+				</p>
+				<h2>1. Coleta de Informações</h2>
+				<p>As informações que coletamos dos usuários e suas justificativas são as seguintes:</p>
+				<ul>
+					<li>
+						<span className="info">XXXX</span>
+						<span className="justify">YYYY</span>
+					</li>
+					<li>
+						<span className="info">XXXX</span>
+						<span className="justify">YYYY</span>
+					</li>
+					<li>
+						<span className="info">XXXX</span>
+						<span className="justify">YYYY</span>
+					</li>
+					<li>
+						<span className="info">XXXX</span>
+						<span className="justify">YYYY</span>
+					</li>
+					<li>
+						<span className="info">XXXX</span>
+						<span className="justify">YYYY</span>
+					</li>
+				</ul>
+			</div>
+		</main>
+	);
+}
