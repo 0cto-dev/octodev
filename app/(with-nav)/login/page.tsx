fc21393ed78cab0fc7ccba0f4272e31ec7a76366
renanrod4 <renanrdemeneses@gmail.com>
2025-12-10 21:31:03 -0300

    feat(login) add popUp de confirmação de entrar sem login

--- app/(with-nav)/login/page.tsx
+++ app/(with-nav)/login/page.tsx
@@ -11,6 +11,8 @@ export default function LoginPage() {
 	const { data: session } = useSession();
 	const [isRedirecting, setIsRedirecting] = useState(false);
 	const [isLoading, setIsLoading] = useState(true);
+	const [isPopUpOpened, setIsPopUpOpened] = useState(false);
+
 	useEffect(() => {
 		if (!window) return;
 
@@ -21,12 +23,34 @@ export default function LoginPage() {
 			setIsLoading(false);
 		}
 	}, [session]);
+
 	if (isRedirecting || isLoading) {
 		return null;
+	}
+	function handleClick(e: React.MouseEvent<HTMLElement, MouseEvent>) {
+		const target = e.target as HTMLElement;
+		if (target.closest('.enterWithoutLogin')) setIsPopUpOpened(true);
+		if (target.closest('.filterDark.active') || target.closest('.cancelBtn')) setIsPopUpOpened(false);
+	}
+	function handleTransition(e: React.TransitionEvent<HTMLElement>) {
+		const target = e.target as HTMLElement;
+		console.log(target);
 	}
 
 	return (
 		<main className="loginPage">
+			<div className={`filterDark ${isPopUpOpened ? 'active' : ''}`} onClick={handleClick}></div>
+			<div className={`popUp ${isPopUpOpened ? 'active' : ''}`}>
+				<p>Tem certeza que deseja entrar sem criar uma conta?</p>
+				<p>Todo o seu progresso durante esta sessão será perdido!</p>
+				<div className="options">
+					<button className="enterWithoutLoginBtn">Sim</button>
+					<button className="cancelBtn" onClick={handleClick}>
+						Não
+					</button>
+				</div>
+			</div>
+
 			<div className="main">
 				<div className="title">
 					<h1>Fazer login</h1>
@@ -50,7 +74,9 @@ export default function LoginPage() {
 						<FaLinkedinIn size={20} />
 						Entrar com LinkedIn
 					</button>
-					<p className="enterWithoutLogin">Entrar sem criar um conta</p>
+					<div className="enterWithoutLogin" onClick={handleClick}>
+						<p>Entrar sem criar um conta</p>
+					</div>
 				</div>
 				<footer>
 					<p>
